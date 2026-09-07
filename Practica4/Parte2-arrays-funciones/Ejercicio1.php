6139df14a1620e4db77c4d06a4dd9147813c097e
Marisol <marisolavila807@gmail.com>
2026-09-06 23:31:39 -0300

    modifico comentario

--- Practica4/Parte2-arrays-funciones/Ejercicio1.php
+++ Practica4/Parte2-arrays-funciones/Ejercicio1.php
@@ -23,11 +23,11 @@ $a['nombre'] = 'manzana';
 $a[] = 4;
 
 ?>
-/*Explicacion:
+/* Explicacion:
 
 En el primer codigo, el array $a se crea de una sola vez, indicando las claves y sus respectivos valores.
 En el segundo codigo, se crea el mismo array agregando cada elemento por separado. Las claves color, sabor, forma y nombre 
 reciben los mismos valores que en el primer caso.
 
 El valor 4 no tiene una clave indicada, por lo que PHP le asigna automáticamente el siguiente índice numérico disponible.
-Conclusión: Ambos codigos son equivalentes porque al finalizar contienen los mismos elementos, con las mismas claves y valores.*/
+Conclusión: Ambos codigos son equivalentes porque al finalizar contienen los mismos elementos, con las mismas claves y valores. */
